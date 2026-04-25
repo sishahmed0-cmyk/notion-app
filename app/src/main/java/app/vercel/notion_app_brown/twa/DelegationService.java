72d21fac0f247d0921aac416d7f4f8397e929797
Sish <sish@gmail.com>
2026-04-25 21:38:58 +0600

    Add app icons

---
+++ app/src/main/java/app/vercel/notion_app_brown/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.notion_app_brown.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
